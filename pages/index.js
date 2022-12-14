68ad813173418e77cf80c2f889b21eb031da3900
ACuteWoof <sutharshanvithushan@gmail.com>
2022-12-15 00:48:21 +0530

    keywords

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,10 @@ function App() {
           name='description'
           content='Have a mind-blowing attractive and personalised website built in a short time. For you.'
         />
+        <meta
+          name='keywords'
+          content='webtrope, webtrope.com, webtrope.com.ng, Web development, Website design, Web design, Web development agency, Web design agency, Website development, Custom website development, Responsive web design, E-commerce website development, User experience (UX) design'
+        />
         <link rel='icon' href='https://github.com/webtrope.png' />
       </Head>
       <main className='min-h-screen dark:bg-main flex justify-between flex-col'>
